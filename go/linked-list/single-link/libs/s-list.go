48dd79a24df3aa263f7e6128074139c0a601a934
Astra-max <waoremaxwel17@gmail.com>
2026-02-05 11:29:04 +0300

    stashed changes

--- go/linked-list/single-link/libs/s-list.go
+++ go/linked-list/single-link/libs/s-list.go
@@ -4,11 +4,11 @@ import "fmt"
 
 type Node struct {
 	data interface{}
-	next *Node
+	Next *Node
 }
 
 type List struct {
-	head *Node
+	Head *Node
 }
 
 func NewList() *List {
@@ -16,55 +16,55 @@ func NewList() *List {
 }
 
 func (l *List) AddNode(data interface{}) {
-	newNode := &Node{data: data, next: nil}
+	newNode := &Node{data: data, Next: nil}
 
-	if l.head == nil {
-		l.head = newNode
+	if l.Head == nil {
+		l.Head = newNode
 		return
 	}
-	newNode.next = l.head
-	l.head = newNode
+	newNode.Next = l.Head
+	l.Head = newNode
 }
 
 func (l *List) Print() {
-	if l.head == nil {
+	if l.Head == nil {
 		return
 	}
-	curr := l.head
+	curr := l.Head
 
 	for curr != nil {
 		fmt.Printf("[%d] ->", curr.data)
-		curr = curr.next
+		curr = curr.Next
 	}
 	fmt.Println("nil")
 }
 
 func (l *List) isEmpty() bool {
-	return l.head == nil
+	return l.Head == nil
 }
 
 func (l *List) Delete() bool {
-	dummy := &Node{data: 0, next: nil}
-	if l.head == nil {
+	dummy := &Node{data: 0, Next: nil}
+	if l.Head == nil {
 		return false
 	}
 
-	if l.head.next == nil {
-		if l.head.data == 0 {
+	if l.Head.Next == nil {
+		if l.Head.data == 0 {
 			return false
 		}
-		l.head = dummy
+		l.Head = dummy
 		return true
 	}
 
-	l.head = l.head.next
+	l.Head = l.Head.Next
 	return true
 }
 
 func (l *List) Insert(pstn int, data interface{}) bool {
 	size := l.Len()
 	newNode := &Node{data, nil}
-	curr := l.head
+	curr := l.Head
 
 	if pstn > size {
 		return false
@@ -72,21 +72,21 @@ func (l *List) Insert(pstn int, data interface{}) bool {
 		l.AddNode(data)
 		return true
 	} else if pstn == size {
-		for curr.next != nil {
-			curr = curr.next
+		for curr.Next != nil {
+			curr = curr.Next
 		}
-		curr.next = newNode
+		curr.Next = newNode
 		return true
 	} else {
 		for curr != nil {
-			next := curr.next
+			next := curr.Next
 
 			if pstn-1 == size {
-				curr.next = newNode
-				newNode.next = next
+				curr.Next = newNode
+				newNode.Next = next
 				return true
 			}
-			curr = curr.next
+			curr = curr.Next
 			size--
 		}
 	}
@@ -94,12 +94,12 @@ func (l *List) Insert(pstn int, data interface{}) bool {
 }
 
 func (l *List) Len() int {
-	curr := l.head
+	curr := l.Head
 	count := 0
 
 	for curr != nil {
 		count++
-		curr = curr.next
+		curr = curr.Next
 	}
 	return count
 }
