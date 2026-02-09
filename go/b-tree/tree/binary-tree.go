a1f08cd02c9a6270539248ec736548f0a91362ff
Astra-max <waoremaxwel17@gmail.com>
2026-02-09 19:12:17 +0300

    preorder tree traversal

--- go/b-tree/tree/binary-tree.go
+++ go/b-tree/tree/binary-tree.go
@@ -1,12 +1,29 @@
 package tree
 
+import (
+	"fmt"
+)
 
 type Node struct {
 	Data interface{}
-}
-
-type Tree struct {
 	Lchild *Node
 	Rchild *Node
 }
 
+type Tree struct {
+	Root *Node
+}
+
+
+func (t *Tree) PreOrder(curr *Node) {
+	if t.IsEmpty() {
+		return
+	}
+	t.PreOrder(curr.Lchild)
+	t.PreOrder(curr.Lchild)
+	fmt.Println(curr.Data)
+}
+
+func (t *Tree) IsEmpty() bool {
+	return t.Root == nil 
+}
