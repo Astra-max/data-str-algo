0e8aefb59259b5c60f3dc66622da19bf4fc4dae1
Astra-max <waoremaxwel17@gmail.com>
2026-02-11 20:13:51 +0300

    implemented selection sort algo

--- go/sorting/main.go
+++ go/sorting/main.go
@@ -7,6 +7,6 @@ import (
 
 func main() {
 	arr := []int{2, 11, 3, 6, 9, 1, 7}
-	val := sort.MergeSort(arr, 0, len(arr)-1)
+	val := sort.SelectionSort(arr)
 	fmt.Println(val)
 }
--- go/sorting/sort/selection-sort.go
+++ go/sorting/sort/selection-sort.go
@@ -1,2 +1,17 @@
 package sort
 
+func SelectionSort(arr []int) []int {
+	n := len(arr)-1
+
+	for i:=0; i<=n; i++ {
+		min := i
+
+		for j:=i+1; j<=n; j++ {
+			if arr[j] < arr[min] {
+				min = j
+			}
+		}
+		arr[i],arr[min] = arr[min], arr[i]
+	}
+	return arr
+}
