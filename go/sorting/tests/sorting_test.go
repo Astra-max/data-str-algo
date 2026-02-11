a8e027aaab1bf0b2c949f7dd5998dc91224da153
Astra-max <waoremaxwel17@gmail.com>
2026-02-11 20:28:01 +0300

    wrote test for insertion sort

--- go/sorting/tests/sorting_test.go
+++ go/sorting/tests/sorting_test.go
@@ -28,3 +28,26 @@ func TestBubble(t *testing.T) {
 		t.Fatalf("Test failed expected %v got %v\n", expected, val)
 	}
 }
+
+
+func TestInsertort(t *testing.T) {
+	arr := []int{2, 11, 3, 6, 9, 1, 7}
+	expected := []int{1, 2, 3, 6, 7, 9, 11}
+
+	val := sort.InsertionSort(arr)
+
+	if !reflect.DeepEqual(expected, val) {
+		t.Fatalf("Test failed expected %v got %v\n", expected, val)
+	}
+}
+
+func TestMergeSort(t *testing.T) {
+	arr := []int{2, 11, 3, 6, 9, 1, 7}
+	expected := []int{1, 2, 3, 6, 7, 9, 11}
+
+	val := sort.MergeSort(arr, 0, len(arr)-1)
+
+	if !reflect.DeepEqual(expected, val) {
+		t.Fatalf("Test failed expected %v got %v\n", expected, val)
+	}
+}
