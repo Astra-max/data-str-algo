9fee76c94d1d6d54934a8c2f89edbcf98e22a23b
Astra-max <waoremaxwel17@gmail.com>
2026-02-15 11:17:40 +0000

    fixed undefined methods in min Stqck struck

--- go/stacks/main.go
+++ go/stacks/main.go
@@ -2,6 +2,7 @@ package main
 
 
 import (
+	"fmt"
 	"stacks/stk"
 	p "stacks/problems"
 )
@@ -10,7 +11,10 @@ func main() {
 	s := stk.NewStack()
 	s.Push(20)
 	s.Push(30)
-	min := p.MinStack(9)
-	min.Print()
+	min := p.MinConst()
+	min.Push(3)
+	min.Push(2)
+	min.Push(5)
+	fmt.Println(min.Min())
 	s.Print()
 }
