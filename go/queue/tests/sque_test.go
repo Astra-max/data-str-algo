390ff3bc046864ba66e7ee91a78e849543584629
Astra-max <waoremaxwel17@gmail.com>
2026-03-15 20:33:34 +0000

    test: basic queue operations

---
+++ go/queue/tests/sque_test.go
@@ -0,0 +1,19 @@
+package tests
+
+
+import (
+	"testing"
+	"que/libs"
+)
+
+func SQue_Test(t *testing.T) {
+	que := libs.NewSQue()
+	que.Push(9)
+	que.Push(3)
+	val, ok := que.Pop()
+	expected := 9
+
+	if ok && expected != val {
+		t.Fatalf("Expected %v but got %v.", expected,val)
+	}
+}
