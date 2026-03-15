394ea8eeb8b46f5ab7e3c2fefa9dd759d663ceb4
Astra-max <waoremaxwel17@gmail.com>
2026-03-15 21:50:35 +0000

    que: update - adding node at the end of list

--- go/queue/tests/que_test.go
+++ go/queue/tests/que_test.go
@@ -6,12 +6,12 @@ import (
 	"testing"
 )
 
-func PeekQue_Test(t *testing.T) {
+func Test_PeekQue(t *testing.T) {
 	que := libs.NewQue()
 	que.Push(3)
 	que.Push(7)
 	val, ok := que.Peek()
-	expected := 8
+	expected := 3
 
 	if ok && val != expected {
 		t.Fatalf("Expected %v but got %v!!.", expected, val)
