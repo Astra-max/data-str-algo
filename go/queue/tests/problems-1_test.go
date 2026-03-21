1db62c6c2c590a866e3c2adc614783c485da02a5
Astra-max <waoremaxwel17@gmail.com>
2026-03-22 00:25:00 +0300

    que-test: tested que-stack pop function

--- go/queue/tests/problems-1_test.go
+++ go/queue/tests/problems-1_test.go
@@ -23,3 +23,23 @@ func Test_QueStackPush(t *testing.T) {
 		t.Fatalf("Expected %v but got %v", expected, got)
 	}
 }
+
+
+func Test_QueStackPop(t *testing.T) {
+	qs := problems.NewQueStack()
+
+	qs.Push(9)
+	qs.Push(2)
+
+	expected := 2
+
+	got, err := qs.Pop()
+
+	if err != nil {
+		t.Fatalf("Expected %v got %v", expected, err)
+	}
+
+	if expected != got {
+		t.Fatalf("Expected %v but got %v", expected, got)
+	}
+}
