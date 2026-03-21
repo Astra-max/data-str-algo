c86757beb8440304414f99e24102f3dbbef81175
Astra-max <waoremaxwel17@gmail.com>
2026-03-22 00:11:37 +0300

    queue: implemented stacks via two ques

--- go/queue/problems/stk-ques.go
+++ go/queue/problems/stk-ques.go
@@ -1,6 +1,7 @@
 package problems
 
 import (
+	"errors"
 	"que/libs"
 )
 
@@ -16,9 +17,10 @@ import (
  }
 
  func NewQueStack() *QueStack {
-	que := new(libs.Que)
+	que1 := new(libs.Que)
+	que2 := new(libs.Que)
 
-	qs := &QueStack{Que1: que, Que2: que}
+	qs := &QueStack{Que1: que1, Que2: que2}
 	return qs
  }
 
@@ -27,7 +29,34 @@ import (
  }
 
  func (qs *QueStack) Peek() (interface{}, error) {
-	return nil,nil
+	if qs.Que1.IsEmpty() {
+		return nil, errors.New("Data not found")
+	}
+
+	queA := qs.Que1
+	queB := qs.Que2
+
+	// move data from que a to b
+	for queA.Size() > 0 {
+		data, found := queA.Pop()
+
+		if found {
+			queB.Push(data)
+		}
+	}
+	data, found := queA.Pop()
+	queB.Push(data)
+
+	// restore queA to avoid corrupting other operations
+
+	for queB.Size() > 0 {
+		data,_ := queB.Pop()
+		queA.Push(data)
+	}
+	if !found {
+		return nil, errors.New("Data not found")
+	}
+	return data,nil
  }
 
  func (qs *QueStack) Pop() (interface{}, error) {
