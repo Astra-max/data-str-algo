fa0198f54bf8830252dd610cb84dc158d1e5c1a0
Astra-max <waoremaxwel17@gmail.com>
2026-03-21 22:28:37 +0300

    stack-test: tested is valid parenthesis func

--- go/stacks/tests/problems-1_test.go
+++ go/stacks/tests/problems-1_test.go
@@ -1,1 +1,15 @@
 package tests
+
+import (
+	"testing"
+	"stacks/problems"
+)
+
+func Test_ValidParenthesis(t *testing.T) {
+	got := problems.ValidParenthesis("([])")
+	expected := true
+
+	if expected != got {
+		t.Fatalf("Expected %v got %v", expected, got)
+	}
+}
