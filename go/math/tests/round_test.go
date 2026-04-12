141a93bbf4aca443e8233a567544ab745cebf3aa
Astra-max <waoremaxwel17@gmail.com>
2026-04-12 20:19:15 +0300

    test: tested mod func and passed test

--- go/math/tests/round_test.go
+++ go/math/tests/round_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestRound(t *testing.T) {
 	expected := 3.0
-	result := libs.Round(2.7)
+	result := libs.Round(2.5)
 	
 	if result != expected {
 		t.Errorf("Expected %f, but got %f", expected, result)
