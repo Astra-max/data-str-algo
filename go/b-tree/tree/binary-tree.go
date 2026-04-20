6895d5edd485070633ab821bc73cd9440764ae0d
Astra-max <waoremaxwel17@gmail.com>
2026-04-20 13:37:01 +0300

    dsa:b-tree: add postordering level ordering func

--- go/b-tree/tree/binary-tree.go
+++ go/b-tree/tree/binary-tree.go
@@ -16,19 +16,20 @@ type Tree struct {
 
 func NewTree(arr []int, low,high int) *Tree {
 	tree := new(Tree)
-	t := CreateTree(arr,low,high)
-	tree.Root = t
+	tree.Root = CreateTree(arr, low, high)
 	return tree
 }
 
 func CreateTree(arr []int, start, size int) *Node {
-	curr := &Node{Data: arr[start]}
 	left := 2 * start + 1
 	right := 2 * start + 2
 
-	if start >= size {
+	if start > size {
 		return nil
 	}
+
+	curr := &Node{Data: arr[start]}
+
 	
 	curr.Lchild = CreateTree(arr, left, size)
 	curr.Rchild = CreateTree(arr, right, size)
@@ -53,6 +54,15 @@ func (t *Tree) Inorder(curr *Node) {
 	t.Inorder(curr.Lchild)
 	fmt.Println(curr.Data)
 	t.Inorder(curr.Rchild)
+}
+
+func (t *Tree) PostOrder(curr *Node) {
+	if curr == nil || t.IsEmpty() {
+		return
+	}
+	t.PostOrder(curr.Lchild)
+	t.PostOrder(curr.Rchild)
+	fmt.Println("value: ", curr.Data)
 }
 
 func (t *Tree) IsEmpty() bool {
